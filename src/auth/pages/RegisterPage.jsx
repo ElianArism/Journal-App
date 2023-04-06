8ea802c2bbc88733c69bf895269a34c3278fe563
ElianArismendi <elian.arismendi@interatica.com>
2023-04-06 17:20:01 -0300

    start implementing register form

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,16 +1,32 @@
 import { Button, Grid, Link, TextField } from "@mui/material";
 import { Link as ReactLink } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 import { AuthLayout } from "../layout/AuthLayout";
 
 export const RegisterPage = () => {
+  const { email, password, name, formState, onInputChange } = useForm(
+    {
+      email: "elian@gmail.com",
+      password: "12354",
+      name: "elian",
+    }
+  );
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  };
   return (
     <AuthLayout title="Register">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               label="full name"
               type="text"
+              name={name}
+              value={name}
+              onChange={onInputChange}
               placeholder="John Doe"
               fullWidth
             />
@@ -19,14 +35,9 @@ export const RegisterPage = () => {
             <TextField
               label="email"
               type="email"
-              placeholder="email@email.com"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField
-              label="email"
-              type="email"
+              name={email}
+              value={email}
+              onChange={onInputChange}
               placeholder="email@email.com"
               fullWidth
             />
@@ -35,6 +46,9 @@ export const RegisterPage = () => {
             <TextField
               label="password"
               type="password"
+              name={password}
+              value={password}
+              onChange={onInputChange}
               placeholder="******"
               fullWidth
             />
@@ -43,7 +57,7 @@ export const RegisterPage = () => {
 
         <Grid container spacing={2} sx={{ my: 2 }}>
           <Grid item xs={12} sm={6}>
-            <Button variant="outlined" fullWidth>
+            <Button variant="outlined" type="submit" fullWidth>
               Login
             </Button>
           </Grid>
